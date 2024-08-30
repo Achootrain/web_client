077c3368913355cd7d971ddd63500adcfa218fcc
Chu Quang Hưng <137665099+Achootrain@users.noreply.github.com>
2024-08-30 09:17:07 +0700

    fix navbar

--- src/App.js
+++ src/App.js
@@ -16,29 +16,28 @@ function App() {
     <div>
     <BrowserRouter>
     <nav class="bg-gray-900 h-14">
-      <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+      <div class="max-w-screen-xl flex flex-wrap items-center justify-between p-4">
         <a href="" class="flex items-center space-x-3 rtl:space-x-reverse">
             <img src="https://png.pngtree.com/png-clipart/20230924/original/pngtree-silver-award-silver-badge-vector-ribbon-banner-decoration-vector-png-image_12584648.png" className="h-8 rounded-full" alt="Flowbite Logo" />
             <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">ACTProManager</span>
         </a>
-        <div class="hidden  w-full md:block md:w-auto" id="navbar-default">
-          <div class="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700 ">
-            <NavLink to="/page/Home"className={({ isActive }) =>`text-base ${isActive ? 'text-emerald-500' : 'text-white'}`}>
-                Home
-            </NavLink>
-            <NavLink to="/page/Work"
-            className={({ isActive }) =>`text-base ${isActive ? 'text-emerald-500' : 'text-white'}`}>
-                Your Project
-            </NavLink>
-            <NavLink to="/page/Login"className={({ isActive }) =>`text-base ${isActive ? 'text-emerald-500' : 'text-white'}`}>
-                Login
-            </NavLink>
-            <NavLink to="/page/Account"className={({ isActive }) =>`text-base ${isActive ? 'text-emerald-500' : 'text-white'}`}>
-                Account
-            </NavLink>
-            <Link to="https://github.com/Achootrain" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-emerald-500 md:p-0 dark:text-white md:dark:hover:text-emerald-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Contact</Link>
-          </div>
+        <div class="font-medium flex rounded-lg flex-row space-x-8 ">
+          <NavLink to="/page/Home"className={({ isActive }) =>`text-base ${isActive ? 'text-emerald-500' : 'text-white'}`}>
+              Home
+          </NavLink>
+          <NavLink to="/page/Work"
+          className={({ isActive }) =>`text-base ${isActive ? 'text-emerald-500' : 'text-white'}`}>
+              Your Project
+          </NavLink>
+          <NavLink to="/page/Login"className={({ isActive }) =>`text-base ${isActive ? 'text-emerald-500' : 'text-white'}`}>
+              Login
+          </NavLink>
+          <NavLink to="/page/Account"className={({ isActive }) =>`text-base ${isActive ? 'text-emerald-500' : 'text-white'}`}>
+              Account
+          </NavLink>
+          <Link to="https://github.com/Achootrain" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-emerald-500 md:p-0 dark:text-white md:dark:hover:text-emerald-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Contact</Link>
         </div>
+    
       </div>
   </nav>
         <Routes>
